45d773d928eded6f92ce8d5f52bb4b7459f7d371
Szabó Ákos <szabo.akos92@gmail.com>
2020-11-19 23:20:28 +0100

    firstArrayList was implemented

--- src/com/company/UrlFixer.java
+++ src/com/company/UrlFixer.java
@@ -1,9 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class UrlFixer {
     public static void main(String[] args) {
         String url = "https//www.reddit.com/r/nevertellmethebots";
-        System.out.println(url.replace("bots", "odds"));
+     //   System.out.println(url.replace("bots", "odds"));
+
+        ArrayList<String> firstArray = new ArrayList<>();
+        System.out.println(firstArray.size());
+        firstArray.add("William");
+        System.out.println(firstArray.isEmpty());
+        firstArray.add("John");
+        firstArray.add("Amanda");
+        System.out.println(firstArray.size());
+        System.out.println(firstArray.get(2));
+        for (int i = 0; i < firstArray.size(); i++) {
+            System.out.println(firstArray.get(i));
+        }
+        firstArray.remove(1);
+        for (int i = firstArray.size() - 1; i >= 0; i--) {
+            System.out.println(firstArray.get(i));
+        }
+        System.out.println("-------------------------------------");
+        for (int i = firstArray.size() - 1; i >= 0; i--) {
+            firstArray.remove(i);
+        }
+        System.out.println(firstArray);
 
     }
 }
