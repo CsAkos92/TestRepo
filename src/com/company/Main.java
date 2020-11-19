1f0855fbd4430308b39bbfc652cc043722b27373
Szabó Ákos <szabo.akos92@gmail.com>
2020-11-19 15:23:02 +0100

    Last array tasks were solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // - Készíts egy `numbers` nevű változót (tömböt)
+        //   a következő tartalommal (értékekkel): `[3, 4, 5, 6, 7]`
+        // - Írasd ki a konzolra a `numbers` tömb összes elemének összegét
+
+        int[] numbers = {3, 4, 5, 6, 7};
+        //System.out.println(numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]);
+
+        // - Készíts egy `numbers` nevű változót (tömböt)
+        //   a következő tartalommal (értékekkel): `[4, 5, 6, 7]`
+        // - Fordítsd meg az elemek sorrendjét a `numbers` tömbben
+        // - Írasd ki a konzolra a `numbers` tömb megfordított sorrendű elemeit
+
+                for (int i = numbers.length - 1; i >= 0 ; i--) {
+                    System.out.println(numbers[i]);
+        }
+
+
+
+
+
+
+
+
     }
 }
