d38494d6d248240b9a9affe7acde9321bfa0c952
Szabó Ákos <szabo.akos92@gmail.com>
2020-11-19 22:56:33 +0100

    Url was fixed

---
+++ src/com/company/UrlFixer.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class UrlFixer {
+    public static void main(String[] args) {
+        String url = "https//www.reddit.com/r/nevertellmethebots";
+        System.out.println(url.replace("bots", "odds"));
+
+    }
+}
